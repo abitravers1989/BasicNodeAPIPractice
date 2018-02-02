e02bbe64914ed8198591c8025b42d9995b87b780
Abigail Travers <abigailtravers@PEG-MAC-007281.local>
2018-02-02 16:50:13 +0000

    Adding view

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const router = express.Router();
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.get("/", function(req, res){
     console.log("Even more nothing");
-    res.send("HIIIIIIS");
+    res.render('firstpage');
 });
 
 app.listen(4000, function() {
